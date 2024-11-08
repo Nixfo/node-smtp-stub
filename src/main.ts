243e6f5278cb2118f849b6878fd4b10526d130b8
Nicolas FONDACCI <ext.nicolas.fondacci@sncf.fr>
2024-11-08 09:30:19 +0100

    ✨ Fastify server

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,57 @@
-import { simpleParser } from 'mailparser';
+import { ParsedMail, simpleParser } from 'mailparser';
 import { SMTPServer } from 'smtp-server';
+import Fastify from 'fastify';
+
+export async function launchWebServer(port: number, host: string) {
+    const fastify = Fastify({
+        logger: true,
+    });
+
+    // Declare a route
+    fastify.get('/', async function handler(_request, _reply) {
+        return { mailCount: emails.length };
+    });
+
+    try {
+        await fastify.listen({ host, port });
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
 
 console.log('Running');
 
+export const emails: ParsedMail[] = [];
+
 const smtpServer = new SMTPServer({
     authOptional: true,
+    onAuth(auth, _session, callback) {
+      console.info(`Received auth from ${auth.username}`);
+      callback(null, {
+        user: auth.username
+      });
+    },
     onData(stream, session, callback) {
-        console.log(`received data from ${session.clientHostname} (${stream.byteLength} bytes)`);
+        console.log(`Received data from ${session.clientHostname} (${stream.byteLength} bytes)`);
         simpleParser(stream)
             .then((email) => {
-              console.log(`received email ${email.html}`);
-            })
+                console.log(`Received email ${email.html}`);
+                emails.push(email);
+                callback();
+            }, callback)
             .catch((e) => {
                 throw new Error('Parsing error', e);
             });
-        callback();
     },
 });
 
+smtpServer.on('error', (err) => {
+    console.error(err);
+});
+
 smtpServer.listen(1025, 'localhost');
+launchWebServer(1024, 'localhost');
+
+//TODO Fastisfy server in a separate file
+//TODO Fastisfy should serve an HTML template
