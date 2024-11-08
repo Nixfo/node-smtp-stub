5695bfca950653e3fbe1d33c0ff4c6dd315aecff
Nicolas FONDACCI <ext.nicolas.fondacci@sncf.fr>
2024-11-08 16:23:00 +0100

    ✨ Fastify endpoints for UI + delete emails

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,6 @@
 import { ParsedMail, simpleParser } from 'mailparser';
 import { SMTPServer } from 'smtp-server';
-import Fastify from 'fastify';
-
-export async function launchWebServer(port: number, host: string) {
-    const fastify = Fastify({
-        logger: true,
-    });
-
-    // Declare a route
-    fastify.get('/', async function handler(_request, _reply) {
-        return { mailCount: emails.length };
-    });
-
-    try {
-        await fastify.listen({ host, port });
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
-}
+import { launchWebServer } from './web-server.js';
 
 console.log('Running');
 
@@ -27,10 +9,10 @@ export const emails: ParsedMail[] = [];
 const smtpServer = new SMTPServer({
     authOptional: true,
     onAuth(auth, _session, callback) {
-      console.info(`Received auth from ${auth.username}`);
-      callback(null, {
-        user: auth.username
-      });
+        console.info(`Received auth from ${auth.username}`);
+        callback(null, {
+            user: auth.username,
+        });
     },
     onData(stream, session, callback) {
         console.log(`Received data from ${session.clientHostname} (${stream.byteLength} bytes)`);
@@ -52,6 +34,3 @@ smtpServer.on('error', (err) => {
 
 smtpServer.listen(1025, 'localhost');
 launchWebServer(1024, 'localhost');
-
-//TODO Fastisfy server in a separate file
-//TODO Fastisfy should serve an HTML template
---
+++ src/web-server.ts
@@ -0,0 +1,39 @@
+import path from 'node:path';
+import fastifyView from '@fastify/view';
+import { Eta } from 'eta';
+import Fastify from 'fastify';
+import { emails } from './main.js';
+import { fileURLToPath } from 'node:url';
+
+// Get the current directory with ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export async function launchWebServer(port: number, host: string) {
+    const fastify = Fastify({
+        logger: true,
+    });
+
+    fastify.register(fastifyView, {
+        engine: {
+            eta: new Eta(),
+        },
+        templates: path.join(__dirname, 'templates'),
+    });
+
+    // Declare a route
+    fastify.get('/', function handler(_request, reply) {
+        return reply.viewAsync('ui.html', { emails });
+    });
+
+    fastify.delete('/', async function handler(_request, _reply) {
+        emails.length = 0;
+    });
+
+    try {
+        await fastify.listen({ host, port });
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
